b1e11e79e361b53ba3156a9d8b0d84bb13ad3a80
Lucas Bartholemy <lucas@bartholemy.com>
2015-05-27 14:55:55 +0200

    Updated translations (translate.humhub.org)

--- messages/da/base.php
+++ messages/da/base.php
@@ -1,11 +1,11 @@
 <?php
 return array (
-  'Back to modules' => '',
+  'Back to modules' => 'Tilbage til moduler',
   'Birthday Module Configuration' => 'Fødselsdag Modul konfiguration',
   'No birthday.' => 'Ingen fødselsdag.',
   'Save' => 'Gem',
-  'The number of days future bithdays will be shown within.' => 'Antallet af dage, fremtidige fødselsdage vil blive vist inden for.',
-  'Tomorrow' => 'I morgen',
+  'The number of days future bithdays will be shown within.' => 'Antallet af dage fremtidige fødselsdage vil blive vist.',
+  'Tomorrow' => 'Imorgen',
   'Upcoming' => 'Kommende',
   'You may configure the number of days within the upcoming birthdays are shown.' => 'Du kan indstille, antallet af dage som vises  i kommende fødselsdage.',
   'becomes' => 'bliver',
