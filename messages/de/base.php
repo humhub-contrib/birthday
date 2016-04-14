0e31178b127600b59053b6af45cebf3d80788496
HumHub Translate <info@humhub.com>
2016-04-14 13:40:05 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,16 +1,13 @@
 <?php
 return array (
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Geburtstage>strong> in den nächsten {days} Tagen',
   'Back to modules' => 'Zurück zu den Modulen',
   'Birthday Module Configuration' => 'Konfiguration Geburtstagsmodul',
+  'In {days} days' => 'In {days} Tagen',
   'Save' => 'Speichern',
   'The number of days future bithdays will be shown within.' => 'Anzahl an Tagen, an denen zukünftige Geburtstage vorab angezeigt werden.',
   'Tomorrow' => 'Morgen',
-  'Upcoming' => 'Demnächst',
   'You may configure the number of days within the upcoming birthdays are shown.' => 'Du kannst die Anzahl an Tagen einstellen, an denen zukünftige Geburstage vorab angezeigt werden.',
-  'becomes' => 'wird',
-  'birthdays' => 'Geburtstage',
-  'days' => 'Tagen',
-  'in' => 'in',
+  'becomes {years} years old.' => 'wird {years} Jahre alt.',
   'today' => 'heute',
-  'years old.' => 'Jahre alt.',
 );
