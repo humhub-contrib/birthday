0f68a41bc4fc9089827d312cdd850e5b52b74a24
HumHub Translations <info@humhub.com>
2018-03-25 21:04:17 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Birthdays</strong> within the next {days} days' => '',
-  'Back to modules' => '',
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Születésnap </strong>a következő {days} napban',
+  'Back to modules' => 'Vissza a modulokhoz',
   'Birthday Module Configuration' => '',
-  'In {days} days' => '',
+  'In {days} days' => '{days} múlva',
   'Save' => 'Mentés',
   'The number of days future bithdays will be shown within.' => '',
-  'Tomorrow' => '',
+  'Tomorrow' => 'Holnap',
   'You may configure the number of days within the upcoming birthdays are shown.' => '',
-  'becomes {years} years old.' => '',
-  'today' => '',
+  'becomes {years} years old.' => '{years} éves lesz.',
+  'today' => 'ma',
 );
