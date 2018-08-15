51d4d436017bcde1983419a297f4280e11f1e7a4
HumHub Translations <info@humhub.com>
2018-08-15 11:52:56 +0200

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/am/base.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    '<strong>Birthdays</strong> within the next {days} days' => '',
+    'Back to modules' => '',
+    'Birthday Module Configuration' => '',
+    'In {days} days' => '',
+    'Save' => '',
+    'The group id of the group that should be exluded.' => '',
+    'The number of days future birthdays will be shown within.' => '',
+    'Tomorrow' => '',
+    'You may configure the number of days within the upcoming birthdays are shown.' => '',
+    'becomes {years} years old.' => '',
+    'today' => '',
+];
