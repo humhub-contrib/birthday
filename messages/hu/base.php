a7db52182fd51d3d0decfb44cf3e8b0ffac437ba
HumHub Translations <info@humhub.com>
2019-01-08 11:46:06 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -5,10 +5,10 @@ return array (
   'Birthday Module Configuration' => 'Születésnap modul konfiguráció',
   'In {days} days' => '{days} napon belül',
   'Save' => 'Mentés',
-  'The group id of the group that should be exluded.' => 'A csoport ID, mely kizárásra kerül.',
-  'The number of days future birthdays will be shown within.' => 'A jövőbeni napok száma, hogy meddig mutassa a születésnapokat.',
+  'The group id of the group that should be exluded.' => 'A kihagyandó csoport csoportazonosítója.',
+  'The number of days future birthdays will be shown within.' => 'Hány napig mutassa a jövőbeni születésnapokat.',
   'Tomorrow' => 'Holnap',
-  'You may configure the number of days within the upcoming birthdays are shown.' => 'Beállíthatod a jövőbeni napok számát, hogy meddig mutassa a születésnapokat.',
+  'You may configure the number of days within the upcoming birthdays are shown.' => 'Itt beállíthatod, hogy hány napig mutassa a jövőbeni születésnapokat.',
   'becomes {years} years old.' => '{years} éves lesz.',
   'today' => 'ma',
 );
