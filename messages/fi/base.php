449493b1e3713f81f7f77181e0a229a21c315aaa
HumHub Translations <info@humhub.com>
2019-03-26 18:55:47 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fi/base.php
+++ messages/fi/base.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Birthdays</strong> within the next {days} days' => '',
-    'Back to modules' => '',
-    'Birthday Module Configuration' => '',
-    'In {days} days' => '',
-    'Save' => '',
-    'The group id of the group that should be exluded.' => '',
-    'The number of days future birthdays will be shown within.' => '',
-    'Tomorrow' => '',
-    'You may configure the number of days within the upcoming birthdays are shown.' => '',
-    'becomes {years} years old.' => '',
-    'today' => '',
-];
+return array (
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Syntymäpäivät</strong> seuraavan {days} päivän aikana',
+  'Back to modules' => 'Takaisin',
+  'Birthday Module Configuration' => '<strong>Syntymäpäivä</strong> laajennuksen hallinta',
+  'In {days} days' => '{days} päivän päästä',
+  'Save' => 'Tallenna',
+  'The group id of the group that should be exluded.' => 'Ryhmä, joka tulisi jättää pois.',
+  'The number of days future birthdays will be shown within.' => 'Voit määrittää kuinka monta päivää ennen syntymäpäivät näkyvät.',
+  'Tomorrow' => 'Huomenna',
+  'You may configure the number of days within the upcoming birthdays are shown.' => 'Voit määrittää kuinka monta päivää ennen syntymäpäivät näkyvät.',
+  'becomes {years} years old.' => 'täyttää {years} vuotta.',
+  'today' => 'tänään',
+);
