8298b636736545586155fb0eefa26ae44c3b24bb
HumHub Translations <info@humhub.com>
2019-11-22 17:20:24 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -5,7 +5,7 @@ return array (
   'Birthday Module Configuration' => 'Configuration du module <strong>anniversaires</strong>',
   'In {days} days' => 'Dans {days} jours',
   'Save' => 'Enregistrer',
-  'The group id of the group that should be exluded.' => 'L\'ID du groupe qui doit être exclu.',
+  'The group id of the group that should be exluded.' => 'L\'ID du groupe à exclure.',
   'The number of days future birthdays will be shown within.' => 'Le nombre de jours avant qu\'un anniversaire soit affiché.',
   'Tomorrow' => 'Demain',
   'You may configure the number of days within the upcoming birthdays are shown.' => 'Vous pouvez spécifier le nombre de jours avant qu\'un anniversaire soit affiché.',
