c8fdd1f998c61c37961ec0e469c44e15154eab01
HumHub Translations <info@humhub.com>
2021-02-23 15:56:58 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  '<strong>Birthdays</strong> within the next {days} days' => '',
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Aniversários</strong> nos próximos {days} dias',
   'Back to modules' => 'Voltar para os módulos',
-  'Birthday Module Configuration' => '',
-  'In {days} days' => '',
+  'Birthday Module Configuration' => 'Configuração do módulo Aniversários',
+  'In {days} days' => 'Em {days} dias',
   'Save' => 'Guardar',
-  'The group id of the group that should be exluded.' => '',
+  'The group id of the group that should be exluded.' => 'A ID de grupo do grupo que deve ser excluído.',
   'The number of days future birthdays will be shown within.' => '',
-  'Tomorrow' => '',
+  'Tomorrow' => 'Amanhã',
   'You may configure the number of days within the upcoming birthdays are shown.' => '',
-  'becomes {years} years old.' => '',
+  'becomes {years} years old.' => 'terá {years} de idade.',
   'today' => 'hoje',
 );
