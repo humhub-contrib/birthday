f4ccb7c8b895675cbb6b5b92f44fb28d06b474df
HumHub Translations <info@humhub.com>
2021-03-24 17:25:21 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -6,9 +6,9 @@ return array (
   'In {days} days' => 'Em {days} dias',
   'Save' => 'Guardar',
   'The group id of the group that should be exluded.' => 'A ID de grupo do grupo que deve ser excluído.',
-  'The number of days future birthdays will be shown within.' => '',
+  'The number of days future birthdays will be shown within.' => 'O número de dias para futuros aniversários será mostrado interiormente.',
   'Tomorrow' => 'Amanhã',
-  'You may configure the number of days within the upcoming birthdays are shown.' => '',
+  'You may configure the number of days within the upcoming birthdays are shown.' => 'Podes configurar como é mostrado o número de dias de entre os próximos aniversários.',
   'becomes {years} years old.' => 'terá {years} de idade.',
   'today' => 'hoje',
 );
