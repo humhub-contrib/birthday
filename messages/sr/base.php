9beb64e9d8a9fdaef787c4e9991bb9e22b368946
HumHub Translations <hello@humhub.com>
2024-02-18 18:19:10 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sr/base.php
+++ messages/sr/base.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Birthdays</strong> within the next {days} days' => '',
-    'Back to modules' => '',
-    'Birthday Module Configuration' => '',
-    'In {days} days' => '',
-    'Save' => '',
-    'The group id of the group that should be exluded.' => '',
-    'The number of days future birthdays will be shown within.' => '',
-    'Tomorrow' => '',
-    'You may configure the number of days within the upcoming birthdays are shown.' => '',
-    'becomes {years} years old.' => '',
-    'today' => '',
-];
+return array (
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Rođendani</strong> u nadolazećih {days} dana',
+  'Back to modules' => 'Povratak na module',
+  'Birthday Module Configuration' => 'Konfiguracija rođendanskog modula',
+  'In {days} days' => 'U {days} dana',
+  'Save' => 'Sačuvaj',
+  'The group id of the group that should be exluded.' => 'ID grupe od grupe koja će biti izostavljena.',
+  'The number of days future birthdays will be shown within.' => 'Broj dana u kojima će se budući rođendani prikazivati.',
+  'Tomorrow' => 'Sutra',
+  'You may configure the number of days within the upcoming birthdays are shown.' => 'Možete konfigurirati broj dana u predstojećim rođendanima.',
+  'becomes {years} years old.' => 'ima {years} godina.',
+  'today' => 'danas',
+);
