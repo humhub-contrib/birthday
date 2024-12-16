b69a8b465489bf4ca2f58a619fc920e304b36399
HumHub Translations <hello@humhub.com>
2024-12-16 09:08:33 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,6 +1,6 @@
 <?php
-return array (
-  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Fönelsedagar</strong> kommande {days} dagar',
+return [
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Födelsedagar</strong> kommande {days} dagar',
   'Back to modules' => 'Tillbaka till moduler',
   'Birthday Module Configuration' => 'Födelsedagsmodul inställningar',
   'In {days} days' => 'Om {days} dagar',
@@ -11,4 +11,4 @@ return array (
   'You may configure the number of days within the upcoming birthdays are shown.' => 'Du kan ställa in hur många dagar som visas under kommande födelsedagar.',
   'becomes {years} years old.' => 'fyller {years} år.',
   'today' => 'idag',
-);
+];
