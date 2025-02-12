8900d8377f7d7b5e06fd4aa4a52397251c6bf746
HumHub Translations <hello@humhub.com>
2025-02-12 10:01:52 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/base.php
+++ messages/pt-BR/base.php
@@ -1,14 +1,14 @@
 <?php
-return array (
-  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Aniversariantes</strong> durantes os próximos {days} dias.',
+return [
+  '<strong>Birthdays</strong> within the next {days} days' => '<strong>Aniversariantes</strong> nos próximos {days} dias',
   'Back to modules' => 'Voltar aos módulos',
   'Birthday Module Configuration' => 'Configuração do Módulo de Aniversário',
-  'In {days} days' => 'Em {days} dias.',
+  'In {days} days' => 'Em {days} dias',
   'Save' => 'Salvar',
   'The group id of the group that should be exluded.' => 'O ID do grupo que deve ser excluído.',
-  'The number of days future birthdays will be shown within.' => 'O número de dias em que os futuros aniversários serão exibidos.',
+  'The number of days future birthdays will be shown within.' => 'O número de dias em que os aniversários futuros serão exibidos.',
   'Tomorrow' => 'Amanhã',
-  'You may configure the number of days within the upcoming birthdays are shown.' => 'Você pode configurar o número de dias antes dos próximos aniversários para que sejam mostrados no calendário.',
-  'becomes {years} years old.' => 'completando {years} anos.',
+  'You may configure the number of days within the upcoming birthdays are shown.' => 'Você pode configurar o número de dias dentro dos quais os próximos aniversários serão exibidos.',
+  'becomes {years} years old.' => 'completa {years} anos',
   'today' => 'hoje',
-);
+];
