1999efddcce6331ca073cd4fd9c5cf31167b0e56
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-02-27 20:57:28 -0500

    Enh: Add modal option for Birthday Overview

--- widgets/BirthdaySidebarWidget.php
+++ widgets/BirthdaySidebarWidget.php
@@ -3,16 +3,15 @@
 namespace humhub\modules\birthday\widgets;
 
 use Yii;
-use humhub\modules\user\models\User;
 use humhub\models\Setting;
+use humhub\modules\user\models\User;
 
 /**
- * BirthdaySidebarWidget displays the users of upcoming birthdays.
+ * BirthdaySidebarWidget displays users with past and upcoming birthdays.
  *
  * It is attached to the dashboard sidebar.
  *
  * @package humhub.modules.birthday.widgets
- * @author Sebastian Stumpf
  */
 class BirthdaySidebarWidget extends \yii\base\Widget
 {
@@ -21,21 +20,38 @@ class BirthdaySidebarWidget extends \yii\base\Widget
         $range = (int) Setting::Get('shownDays', 'birthday');
         $excludedGroup = (int) Setting::Get('excludedGroup', 'birthday');
         $exclusionSql = "";
+
         if ($excludedGroup > 0) {
             $exclusionSql = "NOT profile.user_id IN (SELECT group_user.user_id FROM group_user WHERE group_user.group_id= " . $excludedGroup . ") AND ";
         }
 
-        $nextBirthDaySql = "DATE_ADD(profile.birthday, INTERVAL YEAR(CURDATE())-YEAR(profile.birthday) + IF((CURDATE() > DATE_ADD(`profile`.birthday, INTERVAL (YEAR(CURDATE())-YEAR(profile.birthday)) YEAR)),1,0) YEAR)";
-        $birthdayCondition = $exclusionSql . $nextBirthDaySql . " BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL " . $range . " DAY)";
+        $nextBirthDaySql = "DATE_ADD(profile.birthday, INTERVAL YEAR(CURDATE())-YEAR(profile.birthday) + 
+            IF((CURDATE() > DATE_ADD(`profile`.birthday, INTERVAL (YEAR(CURDATE())-YEAR(profile.birthday)) YEAR)),1,0) YEAR)";
+
+        $birthdayCondition = $exclusionSql . "(
+            " . $nextBirthDaySql . " BETWEEN DATE_SUB(CURDATE(), INTERVAL " . $range . " DAY) AND CURDATE()
+            OR " . $nextBirthDaySql . " BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL " . $range . " DAY)
+        )";
 
         $users = User::find()
-                ->addSelect(['*', 'user.*', 'profile.*', new \yii\db\Expression($nextBirthDaySql . ' as next_birthday')])
-                ->joinWith('profile')
-                ->where($birthdayCondition)
-                ->addOrderBy(['next_birthday' => SORT_ASC])
-                ->active()
-                ->limit(10)
-                ->all();
+            ->addSelect(['*', 'user.*', 'profile.*', new \yii\db\Expression($nextBirthDaySql . ' as next_birthday')])
+            ->joinWith('profile')
+            ->where($birthdayCondition)
+            ->addOrderBy(['next_birthday' => SORT_ASC])
+            ->active()
+            ->limit(10)
+            ->all();
+
+        $pastBirthdays = [];
+        foreach ($users as $user) {
+            if ($this->getDays($user) < 0) {
+                $pastBirthdays[] = [
+                    'name' => Html::encode($user->displayName),
+                    'date' => Yii::$app->formatter->asDate($user->profile->birthday, 'php:F j'),
+                    'age' => $this->getAge($user)
+                ];
+            }
+        }
 
         if (count($users) == 0) {
             return;
@@ -44,6 +60,7 @@ class BirthdaySidebarWidget extends \yii\base\Widget
         return $this->render('birthdayPanel', [
             'users' => $users,
             'dayRange' => $range,
+            'pastBirthdays' => $pastBirthdays
         ]);
     }
 
@@ -51,12 +68,12 @@ class BirthdaySidebarWidget extends \yii\base\Widget
     {
         $now = new \DateTime('now');
         $now->setTime(00, 00, 00);
-        $nextBirthday = new \DateTime(date('y') . '-' . Yii::$app->formatter->asDate($user->profile->birthday, 'php:m-d'));
-        $interval = $now->diff($nextBirthday);
 
+        $nextBirthday = new \DateTime(date('Y') . '-' . Yii::$app->formatter->asDate($user->profile->birthday, 'php:m-d'));
+        $interval = $now->diff($nextBirthday);
         $days = (int) $interval->format('%R%a');
 
-        // Handle turn of year
+        // Handle turn of the year and past birthdays
         if ($days < 0) {
             $nextBirthday->modify('+1 year');
             $days = $nextBirthday->diff($now)->days;
@@ -76,5 +93,4 @@ class BirthdaySidebarWidget extends \yii\base\Widget
 
         return $age;
     }
-
 }
--- widgets/views/birthdayPanel.php
+++ widgets/views/birthdayPanel.php
@@ -1,78 +1,65 @@
 <?php
 
+use yii\helpers\Url;
 use humhub\libs\Html;
 use humhub\widgets\PanelMenu;
-use humhub\modules\birthday\Assets;
-
-/**
- * View File for the BirthdaySidebarWidget
- *
- * @uses User $users the profile data of all the users that have birthday the next days.
- *
- * @package humhub.modules.birthday.widgets.views
- * @author Sebastian Stumpf
- */
+use humhub\modules\birthday\assets\Assets;
 
 $assets = Assets::register($this);
+$enabled = (bool) Yii::$app->getModule('birthday')->settings->get('enabled');
 
 ?>
 <div class="panel panel-default panel-birthday" id="panel-birthday">
-    
     <div class="panel-heading">
         <?= PanelMenu::widget(['id' => 'panel-birthday']); ?>
-        <?= Yii::t('BirthdayModule.base', '<strong>Birthdays</strong> within the next {days} days', ['days' => $dayRange]); ?>
+        <?= Yii::t('BirthdayModule.base', '<strong>Birthdays</strong> within {days} days', ['days' => $dayRange]); ?>
     </div>
     <div class="panel-body">
-        <div id="birthdayContent">
-            <ul id="birthdayList" class="media-list">
-                <?php foreach ($users as $user): ?>
-                    <?php
-                    $remainingDays = $this->context->getDays($user);
-                    ?>
-                    <li class="birthdayEntry">
-                        <a href="<?= $user->getUrl(); ?>">
+        <ul id="birthdayList" class="media-list">
+            <?php foreach ($users as $user): ?>
+                <?php $remainingDays = $this->context->getDays($user); ?>
+                
+                <li class="birthdayEntry">
+                    <?php if ($enabled): ?>
+                        <div class="media">
+                    <?php else: ?>
+                        <!-- Open modal when clicked -->
+                        <a href="<?= Url::to(['/birthday/birthday/modal-profile', 'userId' => $user->id]) ?>" data-target="#globalModal">
                             <div class="media">
-                                <!-- Show user image -->
-                                <img class="media-object img-rounded pull-left" data-src="holder.js/32x32"
-                                     alt="32x32"
+                    <?php endif; ?>
+                                <img class="media-object img-rounded pull-left"
                                      style="width: 32px; height: 32px;"
                                      src="<?= $user->getProfileImage()->getUrl(); ?>">
-                                     <?php if ($remainingDays == 0) : ?>
+
+                                <?php if ($remainingDays == 0): ?>
                                     <img class="media-object img-rounded img-birthday pull-left"
-                                         data-src="holder.js/16x16" alt="16x16"
                                          style="width: 16px; height: 16px;"
                                          src="<?= $assets->baseUrl ?>/cake.png">
-                                     <?php endif; ?>
-                                <!-- Show content -->
+                                <?php endif; ?>
+
                                 <div class="media-body">
                                     <strong><?= Html::encode($user->displayName); ?></strong>
+                                    
                                     <?php
-                                    // show when the user has his birthday
                                     if ($remainingDays == 0) {
                                         echo ' <span class="label label-danger pull-right">' . Yii::t('BirthdayModule.base', 'today') . '</span>';
-                                    } else if ($remainingDays == 1) {
-                                        echo ' <span class="label label-default pull-right">' . Yii::t('BirthdayModule.base', 'Tomorrow') . '</span>';
-                                    } else {
+                                    } elseif ($remainingDays > 0) {
                                         echo ' <span class="label label-default pull-right">' . Yii::t('BirthdayModule.base', 'In {days} days', ['days' => $remainingDays]) . '</span>';
+                                    } else {
+                                        echo ' <span class="label label-info pull-right">' . Yii::t('BirthdayModule.base', '{days} days ago', ['days' => abs($remainingDays)]) . '</span>';
                                     }
-                                    // show the users age if allowed
+
                                     if ($user->profile->birthday_hide_year == '0') {
-                                        echo '<br />' . Yii::t('BirthdayModule.base', 'becomes {years} years old.', ['years' => $this->context->getAge($user)]);
+                                        echo '<br />' . Yii::t('BirthdayModule.base', $remainingDays < 0 ? 'turned {years} years old.' : 'becomes {years} years old.', ['years' => $this->context->getAge($user)]);
                                     }
                                     ?>
                                 </div>
                             </div>
+                    <?php if (!$enabled): ?>
                         </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
-<style type="text/css">
-    .img-birthday {
-        position: absolute;
-        top: 32px;
-        left: 30px;
-    }
-</style>
